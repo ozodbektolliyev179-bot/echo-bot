68122a336f53ec676d6a97ba0e6752ebf27cbcdc
djumanov <djumanovdev@gmail.com>
2026-02-07 19:19:05 +0500

    command

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ def start(update: Update, context: CallbackContext):
     update.message.reply_text(text='Assalomu alaykum!')
 
 
+def randomcat(update: Update, context: CallbackContext):
+    update.message.reply_photo(photo='https://cataas.com/cat')
+
+
+def greet(update: Update, context: CallbackContext):
+    command, name = update.message.text.split(' ')
+    update.message.reply_text(f'salom {name}')
+
+
+def calc(update: Update, context: CallbackContext):
+    command, expression = update.message.text.split(' ')
+    # shu yerda yozing
+
+
 def echo(update: Update, context: CallbackContext):
     update.message.reply_text(text=update.message.text)
 
@@ -22,6 +36,10 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('randomcat', randomcat))
+    dispatcher.add_handler(CommandHandler('greet', greet))
+    dispatcher.add_handler(CommandHandler('calc', calc)) # /calc 3 + 5
+
     dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
